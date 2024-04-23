df8e31354fdfae3bf71062edb356c4eedc213642
Alexsander Gutierrez <alexsandergutierreez@gmail.com>
2024-04-23 18:12:41 -0300

    fix

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let xp = 1000;
 let nivel = "Ferro";
 
 let niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
-let scores = [1000, 2000, 5000, 7000, 8000, 9000, 10000];
+let pontos = [1000, 2000, 5000, 7000, 8000, 9000, 10000];
 let i = 0;
 
-while (xp > scores[i] && i < niveis.length) {
+while (xp > pontos[i]) {
   i++;
 }
 
